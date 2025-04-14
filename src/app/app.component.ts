02019199ec59729ce0fb8a3a4dc9f3b35168404b
luccagoltzman <luccagoltzman@gmail.com>
2025-04-14 10:34:08 -0300

    feat: :zap: Rotas e componentes principais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from './services/user.service';
+import { Theme } from './models/user.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'habit-hunter';
+export class AppComponent implements OnInit {
+  title = 'Habit Hunter';
+  currentTheme: Theme = Theme.LIGHT;
+  
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.user$.subscribe(user => {
+      this.currentTheme = user.settings.theme;
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HabitListComponent } from './components/habit-list/habit-list.component';
+import { HabitCreateComponent } from './components/habit-create/habit-create.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { RewardsComponent } from './components/rewards/rewards.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'habits', component: HabitListComponent },
+  { path: 'create', component: HabitCreateComponent },
+  { path: 'progress', component: ProgressComponent },
+  { path: 'rewards', component: RewardsComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '**', redirectTo: '' }
+];
